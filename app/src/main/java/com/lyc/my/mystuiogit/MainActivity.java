81544224807acd072689656b8dd14468b6ba0436
BlackBuring <liuyaochang@outlook.com>
2016-06-30 15:56:38 +0800

    a new ima

---
+++ app/src/main/java/com/lyc/my/mystuiogit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lyc.my.mystuiogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+       Button button;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
